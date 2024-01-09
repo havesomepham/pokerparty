f98d27c84d71d1eb6bfc526ef3787d3a863904fa
havesomepham <23087717+havesomepham@users.noreply.github.com>
2024-01-09 15:08:19 -0500

    scoreHand implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { CardComponent } from "./card/card.component";
 import { PlayerComponent } from "./player/player.component";
 import { DealerService } from "./dealer.service";
 import { Player } from "./player";
+import { PokerHandResult } from "./pokerHandResult";
 
 @Component({
 	selector: "app-root",
@@ -27,5 +28,13 @@ export class AppComponent {
 		this.dealerService.shuffleDeck();
 		this.dealerService.dealHands();
 		this.dealerService.dealCommunityCards();
+
+		// TODO delete
+		this.dealerService.playerList.forEach((player) => {
+			let pokerHandResult: PokerHandResult = this.dealerService.scoreHand(
+				player.hand
+			);
+			console.log(pokerHandResult);
+		});
 	}
 }
--- src/app/dealer.service.ts
+++ src/app/dealer.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Card, Suit, Rank } from "./card";
 import { Player } from "./player";
+import { PokerHandResult } from "./pokerHandResult";
 
 @Injectable({
 	providedIn: "root",
@@ -25,7 +26,7 @@ export class DealerService {
 	];
 
 	deck: Card[] = [];
-	communinty: Card[] = [];
+	community: Card[] = [];
 
 	getPlayerList(): Player[] {
 		return this.playerList;
@@ -60,7 +61,7 @@ export class DealerService {
 
 	dealCommunityCards(): void {
 		for (let i = 0; i < 5; i++) {
-			this.communinty.push(this.deck.pop()!);
+			this.community.push(this.deck.pop()!);
 		}
 	}
 
@@ -70,6 +71,161 @@ export class DealerService {
 	//   // and return a numeric value representing the strength of the hand
 	//   return { value: Math.random(), description: "Placeholder hand" };
 	// }
+
+	scoreHand(hand: Card[]): PokerHandResult {
+		// return the best poker hand from a set or sets of cards
+		let cards = hand.concat(this.community);
+
+		// start empty
+		let best = this._result(cards);
+
+		// find best hand
+		for (let combination of this._combinations(cards, 5)) {
+			// calculate value of 5 cards
+			let result = this._calculate(combination);
+			if (result.value > best.value) best = result;
+		}
+
+		// finish with best result
+		return best;
+	}
+
+	_result(cards: Card[], name?: string, value?: number): PokerHandResult {
+		return {
+			cards: cards,
+			name: name || "nothing",
+			value: value || 0,
+		};
+	}
+
+	_combinations(cards: Card[], groups: number): Card[][] {
+		// card combinations with the given size
+		let result: Card[][] = [];
+
+		// not enough cards
+		if (groups > cards.length) return result;
+
+		// one group
+		if (groups == cards.length) return [cards];
+
+		// one card in each group
+		if (groups == 1) return cards.map((card) => [card]);
+
+		// everything else
+		for (let i = 0; i <= cards.length - groups; i++) {
+			let head = cards.slice(i, i + 1);
+			let tails = this._combinations(cards.slice(i + 1), groups - 1);
+			for (let tail of tails) {
+				result.push(head.concat(tail));
+			}
+		}
+
+		return result;
+	}
+
+	_ranked(cards: Card[]): Card[][] {
+		// undefined workaround
+		// split cards by rank
+		let result: Card[][] = [];
+
+		for (let card of cards) {
+			let r = card.rank;
+			result[r] = result[r] || [];
+			result[r].push(card);
+		}
+
+		// condense
+		result = result.filter((rank) => !!rank);
+
+		// high to low
+		result.reverse();
+
+		// pairs and sets first
+		result.sort((a, b) => {
+			return a.length > b.length ? -1 : a.length < b.length ? 1 : 0;
+		});
+
+		return result;
+	}
+
+	_isFlush(cards: Card[]): boolean {
+		// all suits match is flush
+		let suit = cards[0].suit;
+
+		for (let card of cards) {
+			if (card.suit != suit) return false;
+		}
+
+		return true;
+	}
+
+	_isStraight(ranked: Card[][]): boolean {
+		// must have 5 different ranks
+		if (!ranked[4]) return false;
+
+		// could be wheel if r0 is 'ace' and r4 is '2'
+		if (
+			ranked[0][0].rank == 14 &&
+			ranked[1][0].rank == 5 &&
+			ranked[4][0].rank == 2
+		) {
+			// hand is 'ace' '5' '4' '3' '2'
+			ranked.push(ranked.shift()!); // why does this exclamation mark work
+			// ace is now low
+			return true;
+		}
+
+		// run of five in row is straight
+		let r0 = ranked[0][0].rank;
+		let r4 = ranked[4][0].rank;
+		return r0 - r4 == 4;
+	}
+
+	_value(ranked: Card[][], primary: number): number {
+		// primary wins the rest are kickers
+		let str = "";
+
+		for (let rank of ranked) {
+			// create two digit value
+			let r = rank[0].rank;
+			let v = (r < 10 ? "0" : "") + r;
+			for (let i = 0; i < rank.length; i++) {
+				// append value for each card
+				str += v;
+			}
+		}
+
+		// to integer
+		return primary * 10000000000 + parseInt(str);
+	}
+
+	_calculate(cards: Card[]): PokerHandResult {
+		// determine value of hand
+
+		let ranked: Card[][] = this._ranked(cards);
+		let isFlush = this._isFlush(cards);
+		let isStraight = this._isStraight(ranked);
+
+		if (isStraight && isFlush && ranked[0][0].rank == 14)
+			return this._result(cards, "royal flush", this._value(ranked, 9));
+		else if (isStraight && isFlush)
+			return this._result(cards, "straight flush", this._value(ranked, 8));
+		else if (ranked[0].length == 4)
+			return this._result(cards, "four of a kind", this._value(ranked, 7));
+		else if (ranked[0].length == 3 && ranked[1].length == 2)
+			return this._result(cards, "full house", this._value(ranked, 6));
+		else if (isFlush)
+			return this._result(cards, "flush", this._value(ranked, 5));
+		else if (isStraight)
+			return this._result(cards, "straight", this._value(ranked, 4));
+		else if (ranked[0].length == 3)
+			return this._result(cards, "three of a kind", this._value(ranked, 3));
+		else if (ranked[0].length == 2 && ranked[1].length == 2)
+			return this._result(cards, "two pair", this._value(ranked, 2));
+		else if (ranked[0].length == 2)
+			return this._result(cards, "one pair", this._value(ranked, 1));
+		else return this._result(cards, "high card", this._value(ranked, 0));
+	}
 
 	cardToString(card: Card): string {
 		let suitSymbol: string;
